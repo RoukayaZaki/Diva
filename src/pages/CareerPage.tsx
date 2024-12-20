8c78107673f4d46452b2b665ea861fbe4ed831de
RoukayaZaki <roka.zaki2002@gmail.com>
2024-12-20 17:32:04 +0200

    Add: first draft of diva's structure

---
+++ src/pages/CareerPage.tsx
@@ -0,0 +1,9 @@
+function CareerPage() {
+  return (
+    <div>
+      <h1>Career Page</h1>
+    </div>
+  );
+}
+
+export default CareerPage;
---
+++ src/pages/PortfolioPage.tsx
@@ -0,0 +1,10 @@
+function PortfolioPage() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>My portfolio will be here</p>
+    </div>
+  );
+}
+
+export default PortfolioPage;
