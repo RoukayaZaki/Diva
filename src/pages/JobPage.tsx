747c21f7ea2c35e55331f0542225d6098ba5c1e0
RoukayaZaki <roka.zaki2002@gmail.com>
2024-12-20 19:39:12 +0200

    Add: routes to pages with id

--- src/pages/JobPage.tsx
+++ src/pages/JobPage.tsx
@@ -1,9 +1,12 @@
+import { useParams } from "react-router-dom";
+
 function JobPage() {
-  return (
-    <div>
-      <h1>Job Page</h1>
-    </div>
-  );
+    const { id } = useParams();
+    return (
+        <div>
+            <h1>Job Page {id}</h1>
+        </div>
+    );
 }
 
 export default JobPage;
