4473680bfba9aeb1203e37dc74d97f0b8badb6bd
RoukayaZaki <roka.zaki2002@gmail.com>
2024-12-20 18:24:23 +0200

    Add: router to website pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import './App.css'
-// TODO: write router here
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import AboutPage from './pages/AboutPage'
-import ContactPage from './pages/ContactPage'
-
 
 function App() {
 
   return (
     <>      
-      <Router>
-        <Switch>
-          <Route path="/" component={HomePage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/contact" component={ContactPage} />
-        </Switch>
-      </Router>
      <h1>React 18 Alpha</h1>
     </>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,70 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage.tsx'
+import AboutPage from './pages/AboutPage.tsx'
+import ContactPage from './pages/ContactPage.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import ServicePage from './pages/ServicePage.tsx'
+import TermsPage from './pages/TermsPage.tsx'
+import CareerPage from './pages/CareerPage.tsx'
+import FAQPage from './pages/FAQPage.tsx'
+import BlogPage from './pages/BlogPage.tsx'
+import PortfolioPage from './pages/PortfolioPage.tsx'
+import ProjectPage from './pages/ProjectPage.tsx'
+import JobPage from './pages/JobPage.tsx'
+
+const BrowserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/about',
+    element: <AboutPage />,
+  },
+  {
+    path: '/contact',
+    element: <ContactPage />,
+  },
+  {
+    path: '/services',
+    element: <ServicePage />,
+  },
+  {
+    path: '/terms',
+    element: <TermsPage />,
+  },
+  {
+    path: 'Careers',
+    element: <CareerPage />,
+  },
+  {
+    path: 'faq',
+    element: <FAQPage />,
+  },
+  {
+    path: 'blogs',
+    element: <BlogPage />,
+  },
+  {
+    path: 'portfolio',
+    element: <PortfolioPage />,
+  },
+  {
+    path: 'project/:id',
+    element: <ProjectPage />,
+  },
+  {
+    path: 'careers/:id',
+    element: <JobPage />,
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={BrowserRouter} />
   </StrictMode>,
 )
