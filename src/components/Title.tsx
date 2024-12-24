e7b2e9991a5c6b480eb018a4a3250fadccb3fdb3
RoukayaZaki <roka.zaki2002@gmail.com>
2024-12-24 18:04:01 +0200

    Add: first part of homepage including header, about, and first part of portfolio

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -7,9 +7,9 @@ type TitleProps = {
 function Title({ title }: TitleProps) {
     const firstLetter = title.charAt(0).toUpperCase();
     const rest = title.slice(1);
-    return <>
+    return <div>
         <span className={styles.titleFirst}>{firstLetter}</span><span className={styles.titleRest}>{rest}</span>
-    </>
+    </div>
         ;
 }
 
