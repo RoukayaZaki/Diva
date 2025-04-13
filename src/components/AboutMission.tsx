900240c0c903f8550b3b7a3704acb75b356ed753
RoukayaZaki <roka.zaki2002@gmail.com>
2025-04-13 12:17:06 +0200

    add: about page mission desktop view

--- src/components/AboutMission.tsx
+++ src/components/AboutMission.tsx
@@ -1,31 +1,32 @@
 import "../assets/styles/aboutmission.css";
 
 function AboutMission() {
-    return (
-        <section className="about-mission-container">
-            <div className="about-mission-content">
-                {/* <div className="about-mission-text-box">
-          <h2 className="about-mission-title">
-            Diva’s Unique Mission to Elevate Brands...
-          </h2>
-        </div> */}
-                <div className="about-mission">
-
-                    <div className="about-mission-image about-mission-image-left" />
-                    <div className="about-mission-right-section">
-                        <div className="about-mission-image about-mission-image-right" />
-                        <p className="about-mission-description">
-                            Our mission is to craft bespoke solutions that captivate audiences and
-                            elevate brands. With a passion for design, sophistication, confidence,
-                            and precision, we meticulously curate brand narratives infused with
-                            finesse and elegance. Our goal is to ensure that every story we tell
-                            serves as a testament to luxury and refinement.
-                        </p>
-                    </div>
-                </div>
+  return (
+    <section className="about-mission-container">
+      <div className="about-mission-content">
+        <div className="about-mission">
+          <div className="about-mission-image about-mission-image-left">
+            <div className="about-mission-text-box">
+                Diva’s Unique Mission to Elevate Brands...
+              {/* <h2 className="about-mission-title">
+              </h2> */}
             </div>
-        </section>
-    );
-};
+          </div>
+          <div className="about-mission-right-section">
+            <div className="about-mission-image about-mission-image-right" />
+            <div className="about-mission-description">
+              Our mission is to craft bespoke solutions that captivate audiences
+              and elevate brands. With a passion for design, sophistication,
+              confidence, and precision, we meticulously curate brand narratives
+              infused with finesse and elegance. Our goal is to ensure that
+              every story we tell serves as a testament to luxury and
+              refinement.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 export default AboutMission;
